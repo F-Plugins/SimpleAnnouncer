95d4a0aa9b1d70f3a431c4cf5dacd06abe89a6e5
Feli <felinegri5@gmail.com>
2021-05-09 19:27:49 -0300

    Add files via upload

---
+++ SimpleAnnouncer/Models/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAnnouncer.Models
+{
+    public class Config
+    {
+        public double Interval { get; set; }
+        public List<AnnouncerMessage> Messages { get; set; }
+    }
+}
---
+++ SimpleAnnouncer/Script.cs
@@ -0,0 +1,63 @@
+﻿using CitizenFX.Core;
+using System.Timers;
+using CitizenFX.Core.Native;
+using System.IO;
+using Newtonsoft.Json;
+using SimpleAnnouncer.Models;
+using System.Collections.Generic;
+
+namespace SimpleAnnouncer
+{
+    public class Script : BaseScript
+    {
+        private Config _Configuration;
+        private int index = 0;
+
+        public Script()
+        {
+            if (!File.Exists($"{API.GetResourcePath("SimpleAnnouncer")}//config.json"))
+            {
+                File.WriteAllText($"{API.GetResourcePath("SimpleAnnouncer")}//config.json", JsonConvert.SerializeObject(new Config
+                {
+                    Interval = 5000,
+                    Messages = new List<AnnouncerMessage>
+                    {
+                        new AnnouncerMessage
+                        {
+                            Author = "^3 [My server] ^7",
+                            Message = "Remember to kill your dad"
+                        },
+                        new AnnouncerMessage
+                        {
+                            Author = "^5 [AHHHHHH] ^1",
+                            Message = "Aliens came here"
+                        }
+                    }
+                }, Formatting.Indented));
+            }
+
+            _Configuration = JsonConvert.DeserializeObject<Config>(File.ReadAllText($"{API.GetResourcePath("SimpleAnnouncer")}//config.json"));
+
+
+            Timer timer = new Timer(_Configuration.Interval);
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if(index >= _Configuration.Messages.Count)
+            {
+                index = 0;
+            }
+
+            TriggerClientEvent("chat:addMessage", new
+            {
+                args = new [] {_Configuration.Messages[index].Author, _Configuration.Messages[index].Message}
+            });
+
+            index++;
+        }
+    }
+}
